5a93a3d6870e6d65e2b38c3c9ccbb459f1d8a226
aminMuktar <aminmuktar41@gmail.com>
2024-03-25 01:05:51 +0300

    final mongo connection and user model migration

---
+++ src/models/user-model.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+  clerkUserId:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name:{
+    type: String,
+    required: true,
+  },
+  userName:{
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+  },
+  profilePicture:{
+    type: String,
+    required: false,
+  },
+  bio:{
+    type: String,
+    required: false
+  }
+
+},{timestamps: true});
+
+if(mongoose.models && mongoose.models["users"]){
+  mongoose.deleteModel("users")
+}
+
+const userModel = mongoose.model("users",userSchema);
+export default userModel
---
+++ src/server-actions/users.ts
@@ -0,0 +1,33 @@
+import { connectMongoDB } from "@/config/db-config";
+import userModel from "@/models/user-model";
+import { currentUser } from "@clerk/nextjs";
+
+connectMongoDB();
+
+export const GetCurrentUserFromMongoDB = async ()=>{
+
+  try {
+    const clerkUser = await currentUser();
+    const mongoUser = await userModel.findOne({ clerkUserId: clerkUser?.id})
+    if(mongoUser){
+      return JSON.parse(JSON.stringify(mongoUser))
+    }
+    let email=""
+    if(clerkUser?.emailAddresses){
+      email=clerkUser?.emailAddresses[0]?.emailAddress || ""
+    }
+    const newUserPayload={
+      clerkUserId: clerkUser?.id,
+      name: clerkUser?.firstName+" "+clerkUser?.lastName,
+      userName: clerkUser?.username,
+      email,
+      profilePicture: clerkUser?.imageUrl
+    }
+    const newUser = await userModel.create(newUserPayload)
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error: any) {
+    return {
+      error: error.message
+    }
+  }
+}
