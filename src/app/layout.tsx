14907070b9510a7a1ade74e44dbe51ca4a81279d
aminMuktar <aminmuktar41@gmail.com>
2024-03-25 23:00:54 +0300

    setup redux toolkit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import ThemeProvider from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import LayoutProvider from "@/providers/layout-provider";
+import ReduxProvider from "@/providers/redux-provider";
 export const metadata: Metadata = {
   title: "Create Next App",
   description: "Real time chat app",
@@ -18,7 +19,9 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <ThemeProvider>
-            <LayoutProvider>{children}</LayoutProvider>
+            <ReduxProvider>
+              <LayoutProvider>{children}</LayoutProvider>
+            </ReduxProvider>
           </ThemeProvider>
         </body>
       </html>
