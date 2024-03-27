d213a9336df8b33da830e3b55d0eb29c9453c5a2
aminMuktar <aminmuktar41@gmail.com>
2024-03-27 11:06:12 +0300

    chat app architecture

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,29 @@ export interface UserType{
   createdAt: string;
   updatedAt: string;
 }
+
+export interface ChatType{
+  _id: string;
+  user: UserType[];
+  createdBy: UserType;
+  lastMessage: MessageType;
+  isGroupChat:boolean;
+  groupName: string;
+  groupProfilePicture: string;
+  groupBio: string;
+  groupAdmins: UserType[];
+  unreadCounts: any;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface MessageType{
+  _id: string;
+  chat: ChatType;
+  sender: UserType;
+  text: string;
+  image: string;
+  readBy: UserType[];
+  createdAt: string;
+  updatedAt: string;
+}
